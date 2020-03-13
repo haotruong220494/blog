cde12f4e63600e3b824b052ae6c6377ed8912209
haotv03 <hao.truong.van@bunbusoft.com>
2020-03-13 18:08:18 +0700

    Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "dashboard#home_page"
   namespace :admin do
     devise_for :admins, path: "", path_names: { sign_in: "login", sign_out: "logout" }, skip: [:registrations, :passwords]
-    root "dashboards#index"
+    root "dashboard#index"
   end
 end
