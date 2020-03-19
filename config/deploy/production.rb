73b403806dffafb05573780cd9d6a914e8f763b1
haotv03 <hao.truong.van@bunbusoft.com>
2020-03-19 18:39:37 +0700

    Deploy testing

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :stage, :production
 set :rails_env, :production
 set :branch, "master"
 set :deploy_to, "/home/deploy/deploy/blog"
-server "54.169.166.126", user: "deploy", roles: %w{app db web}
+server "52.77.122.70", user: "deploy", roles: %w{app db web}
 
